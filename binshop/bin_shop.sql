79eab7d065cd70988f81377526c5715c93a37295
Vipach <101686295+Vipach@users.noreply.github.com>
2024-12-12 12:31:29 +0700

    Add files via upload

---
+++ binshop/bin_shop.sql
@@ -0,0 +1,252 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th4 28, 2021 lúc 07:08 AM
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `binshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `admin_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`id_admin`, `username`, `password`, `admin_status`) VALUES
+(1, 'admin', '25f9e794323b453885f5181f1b624d0b', 1),
+(2, 'binadmin' ,  '25f9e794323b453885f5181f1b624d0b', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_cart`
+--
+
+CREATE TABLE `tbl_cart` (
+  `id_cart` int(11) NOT NULL,
+  `id_khachhang` int(11) NOT NULL,
+  `code_cart` varchar(10) NOT NULL,
+  `cart_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_cart`
+--
+
+INSERT INTO `tbl_cart` (`id_cart`, `id_khachhang`, `code_cart`, `cart_status`) VALUES
+(3, 1, '9281', 0),
+(4, 1, '4458', 0),
+(5, 8, '1632', 1),
+(6, 8, '8138', 0),
+(7, 1, '4175', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_cart_details`
+--
+
+CREATE TABLE `tbl_cart_details` (
+  `id_cart_details` int(11) NOT NULL,
+  `code_cart` varchar(10) NOT NULL,
+  `id_sanpham` int(11) NOT NULL,
+  `soluongmua` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_cart_details`
+--
+
+INSERT INTO `tbl_cart_details` (`id_cart_details`, `code_cart`, `id_sanpham`, `soluongmua`) VALUES
+(7, '9281', 9, 3),
+(8, '9281', 8, 1),
+(9, '4458', 9, 3),
+(10, '4458', 8, 1),
+(11, '1632', 12, 5),
+(12, '8138', 11, 5),
+(13, '4175', 6, 2),
+(14, '4175', 8, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_dangky`
+--
+
+CREATE TABLE `tbl_dangky` (
+  `id_dangky` int(11) NOT NULL,
+  `tenkhachhang` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `diachi` varchar(200) NOT NULL,
+  `matkhau` varchar(100) NOT NULL,
+  `dienthoai` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_dangky`
+--
+
+INSERT INTO `tbl_dangky` (`id_dangky`, `tenkhachhang`, `email`, `diachi`, `matkhau`, `dienthoai`) VALUES
+(1, 'Thanh Bình', 'binhntgch190837@fpt.edu.vn', 'ecohome2 ', '25f9e794323b453885f5181f1b624d0b', '0123456789 '),
+(8, 'abcdef', 'maxime80@example.net', 'landmark72 giao hang', '25f9e794323b453885f5181f1b624d0b', '0123456789');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_danhmuc`
+--
+
+CREATE TABLE `tbl_danhmuc` (
+  `id_danhmuc` int(11) NOT NULL,
+  `tendanhmuc` varchar(100) NOT NULL,
+  `thutu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_danhmuc`
+--
+
+INSERT INTO `tbl_danhmuc` (`id_danhmuc`, `tendanhmuc`, `thutu`) VALUES
+(1, 'Ốp lưng đẹp', 1),
+(3, 'Màn hình điện thoại ', 3),
+(4, 'Kính cường lực', 4),
+(5, 'Phụ kiện trang trí', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_sanpham`
+--
+
+CREATE TABLE `tbl_sanpham` (
+  `id_sanpham` int(11) NOT NULL,
+  `tensanpham` varchar(250) NOT NULL,
+  `masp` varchar(100) NOT NULL,
+  `giasp` varchar(50) NOT NULL,
+  `soluong` int(11) NOT NULL,
+  `hinhanh` varchar(50) NOT NULL,
+  `tomtat` text NOT NULL,
+  `noidung` text NOT NULL,
+  `tinhtrang` int(11) NOT NULL,
+  `id_danhmuc` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_sanpham`
+--
+
+INSERT INTO `tbl_sanpham` (`id_sanpham`, `tensanpham`, `masp`, `giasp`, `soluong`, `hinhanh`, `tomtat`, `noidung`, `tinhtrang`, `id_danhmuc`) VALUES
+(6, 'ốp lưng iphone12', '001', '250000', 3, 'tainghe2.png', 'dasdas', 'dsadadasdas', 4, 1),
+(7, 'tai nghe i12', '002', '350000', 10, 'tainghei12.jpg', 'dasdasdas', 'dasdadasdas', 2, 3),
+(8, 'Kính cường lực', '003', '750000', 10, 'kinhcuongluc.png', 'dasd', 'asdsadasdas', 3, 4),
+(9, 'kính loại 1', '004', '250000', 3, 'kinhloai1.png', 'dsad', 'asdasdasdas', 5, 4),
+(10, 'Dây ngọc 1', '005', '99000', 10, 'dayngoc.png', 'fsdfs', 'fsdfdsfsdf', 2, 5),
+(11, 'ốp lung ipX', '001', '250000', 3, 'oplungiphoneX.png', 'dsdad', 'sadsadasd', 1, 1),
+(12, 'ốp lung 1', '001', '250000', 3, 'oplung1.png', 'dsadasd', 'asdasdas', 1, 1);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Chỉ mục cho bảng `tbl_cart`
+--
+ALTER TABLE `tbl_cart`
+  ADD PRIMARY KEY (`id_cart`);
+
+--
+-- Chỉ mục cho bảng `tbl_cart_details`
+--
+ALTER TABLE `tbl_cart_details`
+  ADD PRIMARY KEY (`id_cart_details`);
+
+--
+-- Chỉ mục cho bảng `tbl_dangky`
+--
+ALTER TABLE `tbl_dangky`
+  ADD PRIMARY KEY (`id_dangky`);
+
+--
+-- Chỉ mục cho bảng `tbl_danhmuc`
+--
+ALTER TABLE `tbl_danhmuc`
+  ADD PRIMARY KEY (`id_danhmuc`);
+
+--
+-- Chỉ mục cho bảng `tbl_sanpham`
+--
+ALTER TABLE `tbl_sanpham`
+  ADD PRIMARY KEY (`id_sanpham`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_cart`
+--
+ALTER TABLE `tbl_cart`
+  MODIFY `id_cart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_cart_details`
+--
+ALTER TABLE `tbl_cart_details`
+  MODIFY `id_cart_details` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_dangky`
+--
+ALTER TABLE `tbl_dangky`
+  MODIFY `id_dangky` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_danhmuc`
+--
+ALTER TABLE `tbl_danhmuc`
+  MODIFY `id_danhmuc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_sanpham`
+--
+ALTER TABLE `tbl_sanpham`
+  MODIFY `id_sanpham` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
---
+++ binshop/pages/footer.php
@@ -0,0 +1,36 @@
+<div class="clear"></div>
+		<div class="footer">
+			<style>
+				.button1 {
+ 
+  background-color: #004A7F;
+ 
+  -webkit-border-radius: 10px;
+ 
+  border-radius: 10px;
+   
+  border: none;
+   
+  color: #FFFFFF;
+   
+  cursor: pointer;
+   
+  display: inline-block;
+   
+  font-family: Arial;
+  
+   
+  padding: 5px 10px;
+   
+  text-align: center;
+   
+  text-decoration: none;
+ 
+}
+			</style>
+			<p class="footer_copyright">Copyright by Thanh_Binh developer 2021</p>
+			<ul class="button1" > 
+				<li>Phone: 0123456789</li>
+				<li>Email contact: binhntgch190837@gmail.com</li>
+			</ul>
+		</div>
